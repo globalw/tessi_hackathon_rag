d45c97d547892c32252ce452f0c77174e96d62c4
Chris <chris@Chriss-MacBook-Pro.local>
2024-11-09 13:38:29 +0100

    1st model

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 from llama_index.embeddings.ollama import OllamaEmbedding
 from llama_index.llms.ollama import Ollama
 from llama_index.core import Settings, VectorStoreIndex, SimpleDirectoryReader
+import pandas as pd
 
 
 class RAG:
@@ -63,8 +64,50 @@ def run_query_sync(query: str) -> str:
     return response_text
 
 
+
+def get_billable_answer(long_answer):
+    # Split the string by lines
+    lines = long_answer.split('\n')
+    # Find the line that starts with 'answer:'
+    for line in lines:
+        if line.startswith("answer:"):
+            answer = line.split(":")[1].strip()
+            break
+    return answer
+
+
+
 if __name__ == "__main__":
-    query = "Subject: IMBL Scanner Breakdown - Immediate Repair Required Dear Support Team, We are experiencing a sudden breakdown of our IMBL Scanner, rendering it non-operational. We request immediate assistance for emergency repairs to restore functionality as soon as possible. Thank you for your prompt attention to this matter. Best regards, [Customer Name]"
-    response = run_query_sync(query)
-    print("\nResponse:")
-    print(response)
+
+    prompt_old = 'Classify this question if the request is under the Service Level Agreement (SLA) or not. Return both the relevant passage in the SLA and if the request is covered or not (yes / no)'
+    prompt = f"""Classify if the following request is under the Service Level Agreement (SLA) or not. Return your reply in the following format:
+            answer: [Billable / Not Billable]
+            context: [relevant passage from the SLA]
+            """
+
+    
+    df_questions = pd.read_csv('test/qa/QA_billability.csv')
+    df_questions['predicted_answer'] = 999
+    #print('questions')
+    #print(questions)
+
+    for case_nb in range(0,df_questions.shape[0]):
+   
+
+        #question = "Subject: IMBL Scanner Breakdown - Immediate Repair Required Dear Support Team, We are experiencing a sudden breakdown of our IMBL Scanner, rendering it non-operational. We request immediate assistance for emergency repairs to restore functionality as soon as possible. Thank you for your prompt attention to this matter. Best regards, [Customer Name]"
+        question = df_questions['question'][case_nb]
+        query = prompt + question
+        print("\n================================")
+        print(f'case_nb', case_nb)     
+        print(question)
+        #print(query)
+        response = run_query_sync(query)
+        print("\nResponse:")
+        print(response)
+        pred_billable = get_billable_answer(response)
+        df_questions['predicted_answer'][case_nb] = pred_billable
+        print(f'expected_answer:', df_questions['expected_answer'][case_nb])
+        print(f'expected_context:', df_questions['context'][case_nb])
+        print("\n--------------------------------")
+
+
