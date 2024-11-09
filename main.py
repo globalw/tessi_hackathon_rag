68dafb5773c5639821f76395b78b7e0c4204223d
Marc <mr.moreaux@gmail.com>
2024-11-09 19:02:48 +0100

    quick commit

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+from llama_index.llms.llama_api import LlamaAPI
+from llama_index.llms.openai import OpenAI
 import os
 from typing import List, Union, Generator, Iterator
 from pydantic import BaseModel
 from llama_index.embeddings.ollama import OllamaEmbedding
 from llama_index.llms.ollama import Ollama
 from llama_index.core import Settings, VectorStoreIndex, SimpleDirectoryReader
+
+import pandas as pd
 
 
 class RAG:
@@ -18,9 +22,12 @@ class RAG:
         self.index = None
 
         self.parameters = self.Parameters(
-            LLAMAINDEX_OLLAMA_BASE_URL=os.getenv("LLAMAINDEX_OLLAMA_BASE_URL", "http://localhost:11434"),
-            LLAMAINDEX_MODEL_NAME=os.getenv("LLAMAINDEX_MODEL_NAME", "llama3.2:3b"),
-            LLAMAINDEX_EMBEDDING_MODEL_NAME=os.getenv("LLAMAINDEX_EMBEDDING_MODEL_NAME", "jina/jina-embeddings-v2-base-en:latest"),
+            LLAMAINDEX_OLLAMA_BASE_URL=os.getenv(
+                "LLAMAINDEX_OLLAMA_BASE_URL", "http://localhost:11434"),
+            LLAMAINDEX_MODEL_NAME=os.getenv(
+                "LLAMAINDEX_MODEL_NAME", "llama3.2:1b"),
+            LLAMAINDEX_EMBEDDING_MODEL_NAME=os.getenv(
+                "LLAMAINDEX_EMBEDDING_MODEL_NAME", "jina/jina-embeddings-v2-base-en:latest"),
         )
 
     def on_startup(self):
@@ -31,7 +38,9 @@ class RAG:
         Settings.llm = Ollama(
             model=self.parameters.LLAMAINDEX_MODEL_NAME,
             base_url=self.parameters.LLAMAINDEX_OLLAMA_BASE_URL,
+            request_timeout=3600.0,
         )
+        # Settings.llm = LlamaAPI(model='llama3.2-3b', api_key=LLM_KEY)
 
         self.documents = SimpleDirectoryReader("test/dataset").load_data()
         self.index = VectorStoreIndex.from_documents(self.documents)
@@ -42,9 +51,23 @@ class RAG:
     def pipe(
         self, user_message: str, model_id: str, messages: List[dict], body: dict
     ) -> Union[str, Generator, Iterator]:
-        query_engine = self.index.as_query_engine(streaming=True)
-        response = query_engine.query(user_message)
-        return response.response_gen
+        for _ in range(9):
+            try:
+                chat_engine = self.index.as_chat_engine()
+
+                question1 = f'You are a company that signed a contract with a client. Find if the following query is included in the service provided by the company. Query: {query}'
+                explanation_response = chat_engine.chat(question1).response
+
+                question2 = f'Based on {explanation_response}, would you say this query is billable, query : {query} ? Answer in one word by "yes" or "no".'
+                in_service_response = chat_engine.chat(question2).response
+
+                if in_service_response.lower() not in ['yes', 'no']:
+                    raise (TypeError('Not the expected output'))
+
+            except:
+                print('Meh...')
+
+        return f'{in_service_response}&&&{explanation_response}'
 
 
 def run_query_sync(query: str) -> str:
@@ -64,7 +87,18 @@ def run_query_sync(query: str) -> str:
 
 
 if __name__ == "__main__":
-    query = "Subject: IMBL Scanner Breakdown - Immediate Repair Required Dear Support Team, We are experiencing a sudden breakdown of our IMBL Scanner, rendering it non-operational. We request immediate assistance for emergency repairs to restore functionality as soon as possible. Thank you for your prompt attention to this matter. Best regards, [Customer Name]"
-    response = run_query_sync(query)
-    print("\nResponse:")
-    print(response)
+    df = pd.read_csv('test/qa/QA_billability.csv')
+    res = []
+
+    for query in df['question']:
+        response = run_query_sync(query)
+        print('\n--')
+        print(f'Query: {query}')
+        print(f"Response: {response}")
+        res.append({
+            'question': query,
+            'expected_answer': 'Not Billable' if response.split('&&&')[0].lower() == 'no' else 'Billable',
+            'context': response.split('&&&')[1],
+            'debug_answer': response.split('&&&')[0]})
+
+    pd.DataFrame(res).to_csv('test/qa/QA.csv')
